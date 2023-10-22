2554f5ca837f74a2c8233281f52e399f37e479c0
Zakaria Muse <zaknvf@Zakarias-MacBook-Air.local>
2023-10-22 02:51:22 -0500

    My first solo application

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    function temperatureConverter(valNum) {
+        valNum = parseFloat(valNum);
+        var far = (Math.floor(valNum-273.15)*1.8)+32;
+        return far
+      }
+    var baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    var apiKey = "1932d81919a1136f19238638d4177838"
+    $("#search").click(function(){
+        var location = $("#location").val();
+        $.ajax({
+            url: `${baseURL}?q=${location}&appid=${apiKey}`,
+            method: "GET",
+            success: function(data){
+                console.log(data)
+                $("#weather-info").text(`Temperature: ` + temperatureConverter(data.main.temp) + " Degrees Fahrenheit")
+                $("#weather-info2").text(`Weather: ${data.weather[0].main}`)
+                $("#weather-info3").text(`Conditions: ${data.weather[0].description}`)
+                $("#weather-info4").text(`Humdity: ${data.main.humidity}`)
+                $("#weather-info5").text(`Wind-speed: ${data.wind.speed} mph`)
+            },
+            error: function(error){
+                $("#error-message").text("Network error: please try again." + error);
+            }
+             
+        });
+    })
+});
